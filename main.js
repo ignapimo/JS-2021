c5bf2ad990fa714d6ff9acb9b456b228bcfcf809
ignapimo <ignapimoar@gmail.com>
2021-10-27 22:40:55 -0300

    update

---
+++ main.js
@@ -0,0 +1,31 @@
+let producto = [
+    {ID: 5657, nombre: "Galaxy note 20 ultra", marca :"Samsung", stock: 10},
+    {ID: 6895, nombre: "Iphone X ", marca :"Apple", stock: 30},
+    {ID: 7523, nombre: "TV 50' Ultra LED", marca :"LG", stock: 5},
+    {ID: 4135, nombre: "Smart watch 20' ", marca :"Samsung", stock: 7},
+    {ID: 2745, nombre: "Auriculares boost ", marca :"Sony", stock: 15},
+
+]
+
+
+
+producto.sort ((a,b) => 
+ {
+    if (a.stock <b.stock) {
+        return - 1;
+    }
+    else if (a.stock >b.stock )  {
+        return 1;
+    } else {
+
+        return 0;
+    }
+
+    
+});
+
+for (const value of producto){
+    console.log(value);
+}
+
+
