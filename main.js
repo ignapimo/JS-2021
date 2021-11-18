8ba06bf7a90f72c8263f733ff11af6e333969636
ignapimo <ignapimoar@gmail.com>
2021-11-18 01:43:34 -0300

    update 8

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let productos = [
-    {ID: 5657, nombre: "Galaxy note 20 ultra", marca :"Samsung", stock: 10},
-    {ID: 6895, nombre: "Iphone X ", marca :"Apple", stock: 30},
-    {ID: 7523, nombre: "TV 50' Ultra LED", marca :"LG", stock: 5},
-    {ID: 4135, nombre: "Smart watch 20' ", marca :"Samsung", stock: 7},
-    {ID: 2745, nombre: "Auriculares boost ", marca :"Sony", stock: 15},
+const productos = [
+    {ID: 5657, nombre: "Galaxy note 20 ultra", marca :"Samsung", stock: 10, img:"/images/note20.jpg"},
+    {ID: 6895, nombre: "Iphone X ", marca :"Apple", stock: 30, img:"/images/iphone.jpg"},
+    {ID: 7523, nombre: "TV 50' Ultra LED", marca :"LG", stock: 5, img:"/images/lg.jpg"},
+    {ID: 4135, nombre: "Smart watch 20' ", marca :"Samsung", stock: 7, img:"/images/watch.jpg"},
+    {ID: 2745, nombre: "Auriculares boost ", marca :"Sony", stock: 15, img:"/images/auri.jpg"},
 
-]
+];
 
 
 
@@ -33,14 +33,14 @@ let contenedorCarrito = document.querySelector("#contenedorCarrito");
 function mostrarProd(array) {
     contenedorProd.innerHTML="";
     for (e of array){
-        contenedorProductos.innerHTML+=`
-        <div class="card" style="width: 18rem;">
+        contenedorProd.innerHTML+=`
+        <div class="card col-2" style="width: 9rem;">
   <img src="${e.image}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${e.ID}</h5>
-    <p class="card-text">${e.stock}</p>
-    <h5 class="card-title">${e.nombre}</h5>
-    <button class="btn btn-primary" onclick=>Agregar</button>
+  <h5 class="card-title">${e.nombre}</h5>
+  <p class="card-text">${e.marca}</p>
+  <h5 class="card-title">${e.stock}</h5>
+    <button class="btn btn-primary" onclick="capturar(${e.id})">Agregar</button>
   </div>
 </div>
         `
@@ -48,10 +48,10 @@ function mostrarProd(array) {
 }
 
 function mostrarCarrito(array){
-    let i=0; 
+    let i=1; 
     contenedorCarrito.innerHTML="";
     for (e of array){
-        contenedorCarrito+=`
+        contenedorCarrito.innerHTML+=`
         <tr>
         <th scope="row">${i++}</th>
         <td>${e.ID}</td>
@@ -81,7 +81,7 @@ function guardarStorage(array){
 }
 
 function capturar(id){
-    let prodcutosSeleccionado= productos.find(e => e.id == id);
+    let prodcutoSeleccionado= productos.find(e => e.id == id);
     guardarStorage(agregarStorage(productoSeleccionado));
     mostrarCarrito(JSON.parse(localStorage.getItem("carrito")));
 }
@@ -91,11 +91,30 @@ function quitar(id) {
     let carritofinal=carrito.filter(e=> e.id !=id );
     guardarStorage(carritofinal);
     mostrarCarrito(JSON.parse(localStorage.getItem("carrito")));
+    sumarProductos();
+}
+
+function sumarProductos (){
+    let suma=0;
+    let productosCarrito= JSON.parse(localStorage.getItem("carrito"));
+    for (e of productosCarrito){
+        suma += e.precio
+    }
+let total=document.querySelector("#totalCarrito").textContent=suma;
 }
 
 
 
 mostrarProd(productos);
+
+if (localStorage.getItem("carrito")){
+    mostrarCarrito(JSON.parse(localStorage.getItem("carrito")));
+    sumarCarrito(JSON.parse(localStorage.getItem("carrito")));
+}
+
+document.querySelector("#filtrar").addEventListener("change",(e)=>{
+    e.marca.value !="" ? mostrarProd(filtrar(productos, e.marca.value)) : mostrarProd(prodcutos);
+})
 
 
 
