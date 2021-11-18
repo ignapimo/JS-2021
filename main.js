4ee4520905bbcc67208c9c648406dbf8e103b9f5
ignapimo <ignapimoar@gmail.com>
2021-11-18 00:05:09 -0300

    update 9

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let producto = [
+let productos = [
     {ID: 5657, nombre: "Galaxy note 20 ultra", marca :"Samsung", stock: 10},
     {ID: 6895, nombre: "Iphone X ", marca :"Apple", stock: 30},
     {ID: 7523, nombre: "TV 50' Ultra LED", marca :"LG", stock: 5},
@@ -9,7 +9,7 @@ let producto = [
 
 
 
-producto.sort ((a,b) => 
+productos.sort ((a,b) => 
  {
     if (a.stock <b.stock) {
         return - 1;
@@ -21,18 +21,93 @@ producto.sort ((a,b) =>
         return 0;
     }
 
+
+
     
 });
 
-for (const value of producto){
+
+let contenedorProd = document.querySelector("#contenedorProductos");
+let contenedorCarrito = document.querySelector("#contenedorCarrito");
+
+function mostrarProd(array) {
+    contenedorProd.innerHTML="";
+    for (e of array){
+        contenedorProductos.innerHTML+=`
+        <div class="card" style="width: 18rem;">
+  <img src="${e.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${e.ID}</h5>
+    <p class="card-text">${e.stock}</p>
+    <h5 class="card-title">${e.nombre}</h5>
+    <button class="btn btn-primary" onclick=>Agregar</button>
+  </div>
+</div>
+        `
+    }
+}
+
+function mostrarCarrito(array){
+    let i=0; 
+    contenedorCarrito.innerHTML="";
+    for (e of array){
+        contenedorCarrito+=`
+        <tr>
+        <th scope="row">${i++}</th>
+        <td>${e.ID}</td>
+        <td>${e.nombre}</td>
+        <td>${e.marca}</td>
+        <td>${e.stock}</td>
+        <td><button class="btn btn-danger" onclick="quitar${e.ID}">X</button></td>
+      </tr>`
+    }
+    contenedorCarrito.innerHTML+=`
+    <tr>
+    <td class="text-center" colspan="3">Total</td>
+    <td colspan="2">$<span id ="totalCarrito">0</span></td>
+    </tr>
+    
+    `
+
+}
+function agregarStorage(producto){
+    let storage= localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
+    storage.push(producto);
+    return storage;
+}
+
+function guardarStorage(array){
+    localStorage.setItem("carrito", JSON.stringify(array));
+}
+
+function capturar(id){
+    let prodcutosSeleccionado= productos.find(e => e.id == id);
+    guardarStorage(agregarStorage(productoSeleccionado));
+    mostrarCarrito(JSON.parse(localStorage.getItem("carrito")));
+}
+
+function quitar(id) {
+    let carrito=JSON.parse(localStorage.getItem("carrito"));
+    let carritofinal=carrito.filter(e=> e.id !=id );
+    guardarStorage(carritofinal);
+    mostrarCarrito(JSON.parse(localStorage.getItem("carrito")));
+}
+
+
+
+mostrarProd(productos);
+
+
+
+
+
+for (const value of productos){
     console.log(value);
 }
 
 let divticket =document.createElement("div");
 divticket.id= 'divticket';
 document.body.appendChild(divticket)
-
-
 
 
 
@@ -43,33 +118,18 @@ botonCargar.addEventListener ("click", () => {
 }
  )
 
- botonCargar.createElement ("click", () => {
-    document.querySelector('h1').textContent = 'Welcome'
-    console.log ("Lorem")
-}
- )
-
-
-console.log (document)
-
-
-
-
 console.log ( document.querySelector('h1'))
-
-
-
 
 
 let divClases = document.getElementById("random")
 
-let parrafo = document.createElement ("p")
-
-parrafo.className ="People"
-parrafo.innerHTML = "Esto es una prueba"
 
 
-divClases.appendChild (parrafo)
+
+$("#formulario").on("submit", function(e){
+    console.log(formulariod
+        );
+})
 
 
 
@@ -85,4 +145,3 @@ const imprimirDatos = () => {
         </tr>
         `
 } )}
-
